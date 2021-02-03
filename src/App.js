aeb7a02307db7fc73953989e2ef8aeb97013bc13
awandev <awal.kurniawan@uin-alauddin.ac.id>
2021-02-03 13:58:19 +0800

    simple list with map

--- src/App.js
+++ src/App.js
@@ -2,51 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 
 
-
-
-const firstBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
-  author: 'awal kurniawan',
-  title: 'I Love you to the moon and back'
-}
-
-const secondBook = {
-  img: 'http://yrama-widya.co.id/wp-content/uploads/2015/12/Sejarah_u__SMA_M_555ee05972414.jpg',
-  author: 'Samsul Farid',
-  title: 'Sejarah untuk SMA/MA kelas X',
-}
+const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+    author: 'awal kurniawan',
+    title: 'I Love you to the moon and back'
+  },
+  {
+    img: 'http://yrama-widya.co.id/wp-content/uploads/2015/12/Sejarah_u__SMA_M_555ee05972414.jpg',
+    author: 'Samsul Farid',
+    title: 'Sejarah untuk SMA/MA kelas X',
+  }
+]
 
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        Yuhuuu !!!
-        </Book>
+      {books.map((book) => {
 
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+        const { img, title, author } = book;
+        return (
+          <Book book={book}></Book>
+        )
+      })}
     </section>
-  )
+  );
 }
 
 const Book = (props) => {
-
-  const { img, title, author, children } = props
+  const { img, title, author } = props.book
 
   return (
     <article className='book'>
       <img src={img} alt='' width='200' height='250' />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   )
 }
